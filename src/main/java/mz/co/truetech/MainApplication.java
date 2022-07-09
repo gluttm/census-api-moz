837588b8a569b55314b5f2fabd5a3314d41ae9d5
gluttm <valentyalberto@gmail.com>
2022-07-09 13:16:52 +0200

    run CommandLineRunner only on test mod

--- src/main/java/mz/co/truetech/MainApplication.java
+++ src/main/java/mz/co/truetech/MainApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import mz.co.truetech.dto.PermissionDTO;
@@ -36,6 +37,7 @@ public class MainApplication {
 	}
 
 	@Bean
+	@Profile("test")
 	CommandLineRunner run(UserService userService,
 						  JwtConfig jwtConfig, ProvinceService provinceService,
 						  RoleService roleService, DistrictService districtService,
